deece385fb3bf2347fe94d71fb12a7902a5b0f6a
Osbert Nelda <onelda@mi-q1.msk.21-school.ru>
2022-06-04 21:39:13 +0300

    all

--- my_mlx_pixel_put.c
+++
@@ -1,9 +0,0 @@
-#include "fdf.h"
-
-void	my_mlx_pixel_put(t_win *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
