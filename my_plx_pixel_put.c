9bfc5230501a4e7416a962372dbb7685317b6f0a
Osbert Nelda <onelda@at-g6.msk.21-school.ru>
2022-06-13 21:32:34 +0300

    fix_bags

--- my_plx_pixel_put.c
+++ my_plx_pixel_put.c
@@ -5,7 +5,7 @@ void	my_mlx_pixel_put(s_struct *data, int x, int y, int color)
 	char	*dst;
 	// if (x >= data->window->win_height || y > data->window->win_width || x < 0 || y < 0)
 	// {
-	// 	return ;
+	// 	return ;dadsa
 	// }
 	dst = data->window->addr + (y * data->window->line_length + x * (data->window->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
